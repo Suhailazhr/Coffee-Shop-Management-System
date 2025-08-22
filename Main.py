829b1cbf8bc39ad94ce3573474f53e2c8f9fc270
Suhail Azhr <suhailazhr01@gmail.com>
2025-08-22 15:35:49 -0700

    Language

--- Main.py
+++ Main.py
@@ -8,10 +8,10 @@ print (menu,"\n")
 order = input("What do you Want.? Hot/Cold Coffee:- ")
 print ("Price of the Coffee... Rs. 150/-","\n")
 
-print("==========How Many Do You Want..!!!==========")
+print("========== Quantity ==========")
 
 coffee_1 = input ("How many do you Want...? ")
 coffee_2 = 150
 print ("Your Bill Is:- ",int(coffee_1)*int(coffee_2),"\n")
 
-print("Thank You For Visit Us..!!!")
+print("✅ Thank you for your order! Please visit us again. ☕")
